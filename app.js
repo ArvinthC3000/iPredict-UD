11743585fb22070c99e3fdb2e951de7bad090295
ajithcravi <craviajith@gmail.com>
2019-10-23 23:43:21 +0530

    bill table created

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ let particulars = [];
 let amount = [];
 
 addItem = () => {
-    let item = document.getElementById("item");
-    let qnty = document.getElementById("qnty");
+    let item = document.getElementById("item").value;
+    let qnty = document.getElementById("qnty").value;
     let bill = document.createElement("table");
-    bill.setAttribute("id", "bill")
+    bill.setAttribute("id", "bill");
     document.body.appendChild(bill);
+
         if (startFlag == false){
-            let tableHead = document.createElement("tr");
-            tableHead.setAttribute("id", "tableHead");
+            let tableRow = document.createElement("tr");
+            tableRow.setAttribute("id", "tableRow");
+            let th = document.createElement("th");
+            th.setAttribute("id", "yy")
             let td = document.createElement("td");
             td.setAttribute("id", "xx")
 
@@ -19,15 +22,30 @@ addItem = () => {
             for(i=0; i<4; i++){
                 let content = document.createTextNode(head[i]);
                 console.log(head[i]);
-                td.appendChild(content);
+                th.appendChild(content);
             }
-            tableHead.appendChild(td);
-            bill.appendChild(tableHead);
+            tableRow.appendChild(th);
+            bill.appendChild(tableRow);
             startFlag += 1;
         }
         else{
             void(0);
         }
+    
+        let tableRow = document.createElement("tr");
+        tableRow.setAttribute("id", "tableRow");
+        let td = document.createElement("td");
+        td.setAttribute("id", "xx")
+
+        let head = [startFlag, item, qnty, 100, 200];
+
+        for(i=0; i<4; i++){
+            let content = document.createTextNode(head[i]);
+            td.appendChild(content);
+        }
+        tableRow.appendChild(td);
+        bill.appendChild(tableRow);
+        startFlag += 1;
 }
 
 document.getElementById("submit").addEventListener("click", addItem);
