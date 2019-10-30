2fe472b097064dc8147c9b1325d154a02b234ce5
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-10-30 10:04:36 +0530

    Scripting

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const mongoose = require('mongoose')
+// const express = require('express')
+// const mongoose = require('mongoose')
 
-const app =  express();
-const PORT = 3001
+// const app =  express();
+// const PORT = 3001
 
-// DB config
+// // DB config
 
-const db = require('./config/key').url
+// const db = require('./config/key').url
 
-// Connect to mongoDB
+// // Connect to mongoDB
 
-mongoose.connect(db, { useNewUrlParser:true, useUnifiedTopology: true })
-.then(()=> console.log('MongoDB connected...'))
-.catch(err=>console.log(err))
+// mongoose.connect(db, { useNewUrlParser:true, useUnifiedTopology: true })
+// .then(()=> console.log('MongoDB connected...'))
+// .catch(err=>console.log(err))
 
-// Bodyparser 
+// // Bodyparser 
 
-app.use(express.urlencoded({ extended:true }))
+// app.use(express.urlencoded({ extended:true }))
 
-app.listen(PORT, console.log('Server is listening at port '+PORT))
+// app.listen(PORT, console.log('Server is listening at port '+PORT))
 
-// Routes
+// // Routes
 
