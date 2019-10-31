0bb07ce25ced85adc0000f5b1a2656f85370e97c
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-10-31 18:25:42 +0530

    api route

--- server/routes/api/datacenter.js
+++ server/routes/api/datacenter.js
@@ -1,3 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const bcrypt =  require('bcryptjs')
+
+
+router.get('/',(req,res)=> console.log("here"))
+
+module.exports = router;
+
