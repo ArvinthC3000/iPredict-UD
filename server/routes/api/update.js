e8e17bfb570a69f17e10080336cdc20e9008b293
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-11-18 20:09:37 +0530

    Update api

--- server/routes/api/update.js
+++ server/routes/api/update.js
@@ -5,14 +5,17 @@ const bcrypt =  require('bcryptjs')
 // User model
 const User = require('../../models/user')
 
-
+// @route   POST api/update
+// @desc    Update item route
+// @access  Public
 router.post('/',(req,res) => {
     const {itemID} = req.body
     User.updateOne({itemID},{
-        $set: {qty:15}
+        $set: {qty:10}
     }, (err,results)=>{
-        console.log(results.result)
+        console.log(itemID)
     })
+    .then(User.findOne({itemID}).then(item => res.json(item)))
     
 })
 
