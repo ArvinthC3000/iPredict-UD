4f1bfd98333b72ca7caa59e0b4daac3bd78942c2
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-11-18 20:08:47 +0530

    responding with predicted value

--- server/routes/api/predict.js
+++ server/routes/api/predict.js
@@ -5,11 +5,28 @@ const bcrypt =  require('bcryptjs')
 const User = require('../../models/user')
 
 router.get('/',(req,res)=>{
-    User.find({ qty: { $lte: 12 } })
-    .then(item =>{
-        if(!item)
-            return res.status(404).json( "no item at critical level" );
-        res.json(item)
+    User.find({}).select({'itemName':1, "_id":0, "previousData":1, "qty":1 , "itemID":1 })
+    .then(item => {
+        if(!item) return console.error(err);
+        let respondString = [];
+        const data = JSON.parse(JSON.stringify(item));
+        const size = data.length 
+        let temp = 0;
+        for(i=0;i<size;i++){
+            let arrayLength = data[i].previousData.length
+            // console.log(data);
+            console.log(data[i].qty,data[i].previousData[arrayLength-1]);
+            if(data[i].qty < data[i].previousData[arrayLength-1]){
+                temp++
+                // console.log("here"+data[i])
+                respondString.push(data[i])
+            }
+            
+        }
+        console.log(respondString)
+        res.json(respondString);
+
+        
     })
 })
 
