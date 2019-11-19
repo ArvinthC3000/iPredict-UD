abf23cb1eb2a052bd1f981e054118dafa31f3257
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-11-19 14:48:54 +0530

    Delete unwanted js file

--- scr/script/homepage.js
+++
@@ -1,8 +0,0 @@
-console.log('Arivnth')
-$("#register").click(function(){
-    window.location.href = './scr/pages/register.html'
-})
-
-$("#login").click(function(){
-    window.location.href = './scr/pages/login.html'
-})
