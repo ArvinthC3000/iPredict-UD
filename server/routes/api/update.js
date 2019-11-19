50ae272af330430ae7a716f5094291170c05c65e
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-11-19 09:26:18 +0530

    Api call to post

--- server/routes/api/update.js
+++ server/routes/api/update.js
@@ -5,19 +5,37 @@ const bcrypt =  require('bcryptjs')
 // User model
 const User = require('../../models/user')
 
-// @route   POST api/update
+// @route   GET api/update
 // @desc    Update item route
 // @access  Public
 router.post('/',(req,res) => {
     const {itemID} = req.body
-    User.updateOne({itemID},{
-        $set: {qty:10}
-    }, (err,results)=>{
-        console.log(itemID)
+    console.log(itemID)
+    User.find({itemID:itemID})
+    .then(item =>{
+        if(!item){
+            console.log("Item id doesn't exists")
+            res.json("Item ID doesn't doesn't exists")
+        } else {
+            console.log(item)
+            res.json(item)
+        }
+
     })
-    .then(User.findOne({itemID}).then(item => res.json(item)))
     
 })
+
+// router.post('/',(req,res) => {
+//     const {itemID} = req.body
+//     console.log(itemID)
+//     User.updateOne({itemID},{
+//         $set: {qty:10}
+//     }, (err,results)=>{
+//         console.log(itemID)
+//     })
+//     .then(User.findOne({itemID}).then(item => res.json(item)))
+    
+// })
 
 
 module.exports = router; 
